6825828ba594882e81a02b0ec9aa9233b500c750
YoniTal6899 <89694679+YoniTal6899@users.noreply.github.com>
2021-12-19 15:43:26 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,15 +1,32 @@
-CC=gcc
-AR=ar
-FLAGS= -Wall -g
-.PHONY: all clean
+# CC=gcc
+# AR=ar
+# FLAGS= -Wall -g
+# .PHONY: all clean
 
+# all: stringProg
+
+# stringProg: main2.c
+# 	$(CC) $(FLAGS) main2.c -o stringProg
+
+# run:
+# 	gcc string_actions.c
+# clean:
+# 	rm -f *.o *.a *.so *.out stringProg
+
+
+CC=gcc
+FLAGS= -Wall -g
+
+#make all
 all: stringProg
 
-stringProg: main2.c
-	$(CC) $(FLAGS) main2.c -o stringProg
+.PHONY: clean
+#make clean
+clean: 
+	rm -f *.o *.a *.so stringProg
 
-run:
-	gcc string_actions.c
-clean:
-	rm -f *.o *.a *.so *.out stringProg
+stringProg: main2.o
+	$(CC) $(FLAGS) -o stringProg main2.c
 
+main2.o: main2.c 
+	$(CC) $(FLAGS) -c main2.c
--- main2.c
+++ main2.c
@@ -1,263 +1,275 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "actions.h"
 
 #define TXT 1024
 #define WORD 30
+#define TRUE 1
+#define FALSE 0
 
-void getWord(char word[WORD]){
-    
-    int counter = 0;
-    char ch = getchar();
-    while(ch!=' ' && ch!='\t' && ch!='\n'){
-        word[counter] = ch;
-        ch = getchar();
-        counter++;
+// help functions
+int isMin(char* word){
+    char start = word[0];
+    char end = word[strlen(word)-1];
+    int res_s = 0;
+    int res_e = 0;
+    if(start < 91 && start > 64){
+        res_s = 1;
+    }
+    else if(start < 123 && start > 96){
+        res_s = 1;
+    }
+    if(end < 91 && end > 64){
+        res_e = 1;
+    }
+    else if(end < 123 && end > 96){
+        res_e = 1;
+    }
+
+    if(res_e == 1 && res_s == 1){
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
 
-int getCharGimatria(char ch){
-    if('a'<=ch && ch<='z'){
-        int value = (ch - 'a' +1);
-        return value;
-    }
-    if('A'<=ch && ch<='Z'){
-        int value = (ch - 'A' +1);
-        return value;
-    }
-    return 0;
-}
-
-
-void getText(char txt[TXT]){
-    
-    int counter = 0;
-    char ch = getchar();
-    while(ch!='~'){
-        txt[counter] = ch;
-        ch = getchar();
-        counter++;
-    }
-}
-
-int getWordGimatria(char word[]){
-    int sum = 0;
-    int i = 0;
-    while (word[i]){
-        sum += getCharGimatria(word[i]);
-        i++;
-    }
-    
-    return sum;
-}
-
-void printGimatriaWords(int value, char txt[]){
-    
-    char to_print[TXT] = {};
-    int txtLength = strlen(txt);
-    
-    for (int i = 0; i < txtLength; i++) {
-        
-        
-        while(getCharGimatria(txt[i]) == 0){
-            i++;
+int countChr(char* str, char c){
+    int res = 0;
+    int i;
+    for(i = 0; i < strlen(str); i++){
+        if(str[i] == c){
+            res++;
         }
-        
-        int j = i;
-        int sum = 0;
-        while(sum < value && j < txtLength){
-            sum+= getCharGimatria(txt[j]);
-            if(sum == value){
-                if (strlen(to_print)>0){
-                    printf("%s~", to_print);    
-                }
-                updateToPrintSTR(txt, i, j, to_print);
-            }
+    }
+    return res;
+}
+
+int isEqualAtbash(char* s, char* recived){
+    int i;
+    int j;
+    for(i = 0, j = 0; i < strlen(s); i++){
+        if(s[i] == ' ' || s[i] == '\n' || s[i] == '\t'){
+            continue;
+        } 
+        else if(s[i] == recived[j]){
             j++;
         }
+        else{
+            return 0;
+        }
     }
-    
-    if (strlen(to_print)>0){
-        printf("%s", to_print);    
+    return 1;
+}
+
+int isEqual(char* s, char* recived){
+    int i;
+    int cnt_rec;
+    int cnt_s;
+    for(i = 0; i < strlen(recived); i++){
+        if(recived[i] == ' ') continue;
+        cnt_rec = countChr(recived, recived[i]);
+        cnt_s = countChr(s, recived[i]);
+
+        if(cnt_rec != cnt_s){
+            return 0;
+        }
+    }
+    for(i = 0; i < strlen(s); i++){
+        if(s[i] == ' ') continue;
+        cnt_rec = countChr(recived, s[i]);
+        cnt_s = countChr(s, s[i]);
+
+        if(cnt_rec != cnt_s){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int toGematri(char* word){ 
+    int lower = 96;
+    int uper = 64;
+    int res = 0;
+    int i;
+    for(i = 0; i < strlen(word); i++){
+        char c = word[i];
+        if(c > 96 && c < 123){
+            res += (c - lower); // using ascii values
+        }
+        if(c > 64 && c < 91){
+            res += (c - uper); // using ascii values
+        }
+    }
+    return res;
+}
+
+char* reverse(char* word){
+    char* w = (char*) malloc(WORD);
+    if(w){
+        int i;
+        for(i = 0; i < strlen(word); i++){ 
+            w[i] = (char) word[strlen(word) - 1 - i];
+        }
+        return w;
+    }
+    else{
+        return NULL;
     }
 }
 
-
-void updateToPrintSTR(char txt[], int i, int j, char to_print[]){
-    int index = 0;
-    for (; i <= j; i++) {
-        to_print[index] = txt[i];
-        index++;
-    }
-    to_print[index] = 0;
-}
-
-
-void changeAtbash(char str[]){
-    for (int i = 0; i < strlen(str); i++) {
-        if('a'<=str[i] && str[i]<='z'){
-            str[i] = ('z'- str[i] + 'a');
-        }
-        
-        if('A'<=str[i] && str[i]<='Z'){
-            str[i] = ('Z'- str[i] + 'A');
-        }
-    }
-}
-
-
-void printAtbashWords(char word[], char txt[]){
-    
-    char to_print[TXT] = {};
-    int txtLength = strlen(txt);
-    char atbashWord[strlen(word)];
-    strcpy(atbashWord, word);
-    changeAtbash(atbashWord);
-    int value = getWordGimatria(atbashWord);
-    
-    for (int i = 0; i < txtLength; i++) {
-        
-        while(txt[i] == ' '){
-            i++;
-        }
-        
-        int j = i;
-        int sum = 0;
-        while(sum < value && j < txtLength){
-            sum+= getCharGimatria(txt[j]);
-            
-            if(sum == value){
-                if(sameLettersOrder(txt, i, j, atbashWord) || reverseLettersOrder(txt, i, j, atbashWord)){
-                    
-                    if (strlen(to_print)>0){
-                        printf("%s~", to_print);
+// A
+void Gematria(char* recieved, char* txt){
+    int gimetric_recieved = toGematri(recieved);
+    int i;
+    int j;
+    int start = 0;
+    for(i = 0; i < strlen(txt); i++){
+        char* curr = txt+i;
+        for(j = strlen(curr) - 1; j >= 0 ; j--){
+            char* copy = (char*) malloc(TXT);
+            strncpy(copy, curr, j);
+            if(isMin(copy) == 1){
+                int g = toGematri(copy);
+                if(g == gimetric_recieved){
+                    if(start == 0){
+                        printf("%s", copy);
+                        start++;
                     }
-                    updateToPrintSTR(txt, i, j, to_print);
+                    else{
+                        printf("~%s", copy);
+                    }
                 }
             }
-            j++;
+            free(copy);
         }
-    }
-    
-    if (strlen(to_print)>0){
-        printf("%s", to_print);    
     }
 }
 
-
-int sameLettersOrder(char txt[], int i, int j, char word[]){
-    
-    int counter = 0;
-    for(;i <= j; i++){
-        
-        if(txt[i] != ' '){
-            if(txt[i] != word[counter]){
-                return 0;
+// B
+char* atbash(char* word){
+    char* w = (char*) malloc(WORD);
+    if(w){
+        int i;
+        for(i = 0; i < strlen(word); i++){
+            if(word[i] > 96 && word[i] < 123){
+                w[i] = (char) (123 - word[i] + 96);
             }
-            counter++;
-        }
-    }
-    return 1;
-}
-
-
-int reverseLettersOrder(char txt[], int i, int j, char word[]){
-    int counter = 0;
-    for(;j >= i; j--){
-
-        if(txt[j] != ' '){
-            if(txt[j] != word[counter]){
-                return 0;
+            if(word[i] > 64 && word[i] < 91){
+                w[i] = (char) (91 - word[i] + 64);
             }
-            counter++;
         }
+        return w;
     }
-    return 1;
-}
-
-
-void deleteChar(char str[], int index){
-    while(str[index] != 0){
-        str[index] = str[index+1];
-        index++;
+    else {
+        return NULL;
     }
 }
+void Atbash(char* recieved, char* txt){
+    int i;
+    int j;
+    int start = 0;
 
+    char* atba = atbash(recieved);
+    char* revers_atba = reverse(atbash(recieved));
 
-void printAnagram(char word[], char txt[]){
-    
-    char to_print[TXT] = {};
-    int txtLength = strlen(txt);
-    int word_size = strlen(word);
-    
-    for (int i = 0; i < txtLength; i++) {
-        
-        while(txt[i] == ' '){
-            i++;
-        }
-        
-        int index = i;
-        
-        char copy_word[word_size];
-        strcpy(copy_word, word);
-        int loc = 0;
-        
-        while(strlen(copy_word) > 0 && index < txtLength){
-            
-            if(txt[index] == ' '){
-                index++;
-                continue;
+    for(i = 0; i < strlen(txt); i++){
+        char* curr = txt+i;
+        for(j = strlen(curr) - 1; j >= 0 ; j--){
+            char* copy = (char*) malloc(TXT);
+            strncpy(copy, curr, j);
+            if(isMin(copy) == 1){
+                if(isEqual(copy, atba) == 1 || isEqual(copy, revers_atba) == 1){
+                    if(isEqualAtbash(copy, atba) == 1 || isEqualAtbash(copy, revers_atba) == 1){
+                        if(start == 0){
+                            printf("%s", copy);
+                            start++;
+                        }
+                        else{
+                            printf("~%s", copy);
+                        }
+                    }
+                }
             }
-            loc = getLocation(copy_word, txt[index]);
-            if (loc == -1){
-                break;
+            free(copy);
+        }
+    }
+}
+
+// C
+void isAnagram(char* recieved, char* txt){
+    int gimetric_recieved = toGematri(recieved);
+    int i;
+    int j;
+    int start = 0;
+    for(i = 0; i < strlen(txt); i++){
+        char* curr = txt+i;
+        for(j = strlen(curr) - 1; j >= 0 ; j--){
+            char* copy = (char*) malloc(TXT);
+            strncpy(copy, curr, j);
+            if(isMin(copy) == 1){
+                int g = toGematri(copy);
+                if(g == gimetric_recieved && isEqual(copy, recieved) == 1){
+                    if(start == 0){
+                        printf("%s", copy);
+                        start++;
+                    }
+                    else{
+                        printf("~%s", copy);
+                    }
+                }
+            }
+            free(copy);
+        }
+    }
+}
+
+int main(){
+    char* word = (char*) malloc(WORD);
+    if(word){
+        int i;
+        for(i = 0; i < WORD; i++){
+            char c = getchar();
+            if(c != '\t' && c != '\n' && c != ' '){
+                word[i] = c;
             }
             else{
-                deleteChar(copy_word, loc);
-                index++;
+                word[i] = '\0';
+                break;
             }
         }
-        if(strlen(copy_word) == 0){
-            if (strlen(to_print)>0){
-                printf("%s~", to_print);    
+    }
+
+    char* txt = (char*) malloc(TXT);  
+    if(txt){
+        int j;
+        for(j = 0; j < TXT; j++){
+            char c2 = getchar();
+            if(c2 != '~'){
+                txt[j] = c2;
             }
-            updateToPrintSTR(txt , i, index-1, to_print);
+            else{
+                txt[j] = '~';
+                txt[j+1] = '\0';
+                break;
+            }
         }
     }
-    if (strlen(to_print)>0){
-        printf("%s", to_print);    
-    }
-}
 
-int getLocation(char word[], char ch){
-    int index = -1;
-    for (int i = 0; i < strlen(word) && word[i] != 0; i++) {
-        if(word[i] == ch){
-            index = i;
-            return index;
-        }
-    }
-    return index;
-}
-
-int main()
-{
-    char w[WORD] = {};
-    char t[TXT] = {};
-    
-    getWord(w);
-    getText(t);
-    
-    int wordValue = getWordGimatria(w);
-
+    // printf("\n");
     printf("Gematria Sequences: ");
-    printGimatriaWords(wordValue, t);
-    
-    printf("\nAtbash Sequences: ");
-    printAtbashWords(w, t);
-    
-    printf("\nAnagram Sequences: ");
-    printAnagram(w, t);
-    
+    Gematria(word,txt);
+    printf("\n");
+
+    printf("Atbash Sequences: ");
+    Atbash(word, txt);
+    printf("\n");
+
+    printf("Anagram Sequences: ");
+     isAnagram(word,txt);
+    // printf("\n");
+
+    free(word);
+    free(txt);
     return 0;
 }
